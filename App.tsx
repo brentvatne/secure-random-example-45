11af0678fe6f101f5189cfbad49abb57c03a0c09
ramiel99 <ramielwan48@gamil.com>
2022-04-28 18:47:03 +0300

    repro done

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-get-random-values'
+import {v4} from 'uuid';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{v4()}</Text>
       <StatusBar style="auto" />
     </View>
   );
